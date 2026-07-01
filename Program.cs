2229bbcd19f057fd802a7335198cf6e3cf1d395f
keagmil <Keagmil@gmail.com>
2026-07-01 17:12:37 -0600

    Issues

--- Program.cs
+++ Program.cs
@@ -27,16 +27,30 @@ Console.Clear();
 
 string originalStory = "A vacation is when you take a trip to some (adjective) place with your (adjective) family. Usually, you go to some place that is near a/an (noun) or up on a/an (noun). A good vacation place is one where you can ride (plural noun) or play (game) or go hunting for (plural noun). I like to spend my time (verb ending in “ing”) or (verb ending in “ing”). When parents go on a vacation, they spend their time eating three (plural noun) a day, and fathers play golf, and mothers sit around (verb ending in “ing”) Last summer, my little brother fell in a/an (noun) and got poison (plant) all over his (part of the body) My family is going to go to (place) and I will practice (verb ending in “ing”) Parents need vacations more than kids because parents are always very (adjective) and because they have to work (number) hours every day all year making enough (plural noun) to pay for the vacation.";
 string[] storyWords = originalStory.Split(' ');
-//char[] letters = storyWords.ToCharArray();
 
-//for (int i = 0; i < storyWords.Length; i++)
 {
-    //Console.WriteLine($"index {i} + Letter {storyWords[i]}");
-    foreach (string firstLetter in storyWords)
-    {
-    Console.WriteLine($" first Letter {firstLetter[0]}");
-    }
-}
+    string newStory = ""; //I was getting issues here. By assigning it an empty value we essentiall can buidl the story again word by word and get around it not being assigned a variable.
 
-//storyWords.Contains("(");
-//Console.Write(storyWords);
+    foreach (string word in storyWords)
+    {
+
+    if (word.Contains('('))
+    {
+        int start = word.IndexOf('(');
+        int end = word.IndexOf(')');
+        string placeholder = word.Substring(start + 1, end - start - 1); //Gets rid of "()" = -/+ 1
+        //"(adjective)" and others now are a variable and replaceable
+        Console.WriteLine($"Give me a(n) {placeholder}");
+        string userInput = Console.ReadLine();
+        string filledWord = word.Replace($"{placeholder}", userInput);
+        newStory += filledWord + " ";
+    }
+    else
+    {
+        newStory += word + " ";
+    }
+    }
+    Console.Write(newStory);
+
+
+}
